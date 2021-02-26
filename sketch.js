05fdb8cb9d458f3e5d19627ec2c07cee1dd3742c
sehath-whjr <sehathj@gmail.com>
2021-02-25 18:58:04 -0700

    Firebase Realtime Database

--- sketch.js
+++ sketch.js
@@ -1,24 +1,28 @@
 var ball;
+var database, position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +30,21 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("error in the coding");
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x' : position.x + x, 
+        'y' : position.y + y
+    });
 }
